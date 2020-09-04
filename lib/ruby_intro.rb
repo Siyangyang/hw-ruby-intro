0ee76d479b9ef16f8617dbf3d6a9d5b526fe7ef4
Ubuntu <ubuntu@ip-172-31-92-82.ec2.internal>
2020-09-04 22:05:33 +0000

    Test commit

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,102 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  # inject and reduce are aliases
+  # empty? string->no character array->no item
+  # :+ :* are enum symbol
+  if arr.empty?
+    return 0
+  end
+  # arr.inject(0){|sum, x| sum + x}
+  #arr.inject(:+)
+  arr.reduce(:+)
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  if arr.empty?
+    return 0
+  elsif arr.length == 1
+    return arr[0]
+  else
+    arr.max(2).reduce(:+)
+  end
+  
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if arr.length < 2
+    return false
+  else
+    arr.each do |i|
+      tmp = n - i
+      #remaining equal to current element
+      if (tmp == i && arr.count(tmp) > 1)
+        return true
+      #find available element
+      elsif tmp != i && arr.include?(tmp)
+        return true
+      end
+    end
+    #No match
+    return false
+  end    
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}" 
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  #i ignore case
+  if s[0] =~ /^[a-z]+$/i && s[0] !~ /^[aeiou]+$/i
+    return true
+  else
+    return false
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  #^ start of the line
+  #$ end of line
+  #/pattern/
+  #* repeat 0 or more times
+  #(00) matches
+  if s =~ /(^[01]*(00)$|^0$)/
+    return true
+  end
+  return false
+  
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  
+  attr_accessor :isbn
+  attr_accessor :price
+  
+  def initialize(isbn, price)
+    if isbn.empty? || price <= 0
+      raise ArgumentError
+    end
+    @isbn = isbn
+    @price = price
+  end
+  
+  # def isbn
+  #   @isbn
+  # end
+  
+  # def price
+  #   @price
+  # end
+  
+  def price_as_string
+    '$%.2f'% @price
+  end
+  
+    
 end
