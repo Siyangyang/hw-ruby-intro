4378962281e1a765f74f52024518f52a2c202398
Ubuntu <ubuntu@ip-172-31-92-82.ec2.internal>
2020-09-07 07:06:41 +0000

    Final Commit and Correct log file

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -15,12 +15,13 @@ def sum arr
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  
   if arr.empty?
     return 0
   elsif arr.length == 1
     return arr[0]
   else
+    #max() find the maximum terms
     arr.max(2).reduce(:+)
   end
   
@@ -36,7 +37,7 @@ def sum_to_n? arr, n
       if (tmp == i && arr.count(tmp) > 1)
         return true
       #find available element
-      elsif tmp != i && arr.include?(tmp)
+      elsif (tmp != i && arr.include?(tmp))
         return true
       end
     end
@@ -52,8 +53,15 @@ def hello(name)
 end
 
 def starts_with_consonant? s
+  # =~ matches the regular expression against a string
+  # return offset of string or nil
   #i ignore case
-  if s[0] =~ /^[a-z]+$/i && s[0] !~ /^[aeiou]+$/i
+  #^ start of the line
+  #$ end of line
+  #/pattern/
+  #hyphen (-)  an inclusive range of characters
+  #Both =~ and match works
+  if (s[0] =~ /^[a-z]$/i && /^[aeiou\W]/i.match(s[0]) == nil  )
     return true
   else
     return false
@@ -97,7 +105,7 @@ class BookInStock
   # end
   
   def price_as_string
-    '$%.2f'% @price
+    "$#{'%.2f' % @price}"
   end
   
     
